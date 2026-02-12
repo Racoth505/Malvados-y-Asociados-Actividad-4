352239b9a28f0589003c1431027fb1d1cb7fd551
Racoth505 <rafafl2001@gmail.com>
2026-02-12 10:48:11 -0600

    Corregir Modal

--- Backend/db.js
+++ Backend/db.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2/promise');
 const db = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: '12345678',
-  database: 'Stocko'
+  password: 'Racoth6534-',
+  database: 'stocko'
 });
 
 module.exports = db;
